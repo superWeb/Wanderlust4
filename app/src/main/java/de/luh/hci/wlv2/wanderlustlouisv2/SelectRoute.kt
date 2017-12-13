14b8f5aabb42906c56b12206e7ff9633301c1ec1
DevWeb <microwin2@gmail.com>
2017-12-13 20:56:53 +0100

    Initial commit

---
+++ app/src/main/java/de/luh/hci/wlv2/wanderlustlouisv2/SelectRoute.kt
@@ -0,0 +1,12 @@
+package de.luh.hci.wlv2.wanderlustlouisv2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectRoute : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_route)
+    }
+}
